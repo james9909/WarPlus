cc47ee7717740cb37378daeff6833fc54df339fe
James Wang <jameswang9909@hotmail.com>
2021-01-06 12:27:06 -0500

    Lint

--- src/main/kotlin/com/github/james9909/warplus/listeners/MagicSpellsListener.kt
+++ src/main/kotlin/com/github/james9909/warplus/listeners/MagicSpellsListener.kt
@@ -8,8 +8,6 @@ import org.bukkit.event.Listener
 import com.nisovin.magicspells.events.SpellCastEvent
 import com.nisovin.magicspells.events.SpellTargetEvent
 import com.nisovin.magicspells.events.SpellTargetLocationEvent
-import com.nisovin.magicspells.events.MagicSpellsBlockBreakEvent
-import com.nisovin.magicspells.events.MagicSpellsBlockPlaceEvent
 import com.nisovin.magicspells.spells.targeted.PulserSpell
 import org.bukkit.Location
 import org.bukkit.entity.Player
