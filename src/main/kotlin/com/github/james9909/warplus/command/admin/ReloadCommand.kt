2e8a134e823448627c6f9cdf56e030d5f8c3f666
James Wang <jameswang9909@hotmail.com>
2021-01-18 15:38:18 -0500

    Allow admins to reload specific warzones instead of reloading the entire plugin (#120)

--- src/main/kotlin/com/github/james9909/warplus/command/admin/ReloadCommand.kt
+++ src/main/kotlin/com/github/james9909/warplus/command/admin/ReloadCommand.kt
@@ -3,16 +3,18 @@ package com.github.james9909.warplus.command.admin
 import com.github.james9909.warplus.WARPLUS_BASE_COMMAND
 import com.github.james9909.warplus.WarPlus
 import com.github.james9909.warplus.util.Message
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
 import org.bukkit.command.CommandSender
 
 class ReloadCommand : AdminCommand() {
     override val usageString = "/$WARPLUS_BASE_COMMAND reload"
     override val description = "Unload and reload the plugin"
 
-    override fun execute(plugin: WarPlus, sender: CommandSender, args: List<String>): Boolean {
+    private fun resetPlugin(plugin: WarPlus, sender: CommandSender) {
         if (!plugin.loaded.get()) {
             plugin.playerManager.sendMessage(sender, Message.ALREADY_UNLOADED)
-            return true
+            return
         }
         plugin.playerManager.sendMessage(sender, Message.UNLOADING_WAR_START)
         plugin.disable()
@@ -20,10 +22,35 @@ class ReloadCommand : AdminCommand() {
         plugin.playerManager.sendMessage(sender, Message.LOADING_WAR_START)
         plugin.initialize()
         plugin.playerManager.sendMessage(sender, Message.LOADING_WAR_DONE)
+    }
+
+    private fun resetWarzone(plugin: WarPlus, sender: CommandSender, warzoneName: String) {
+        if (!plugin.warzoneManager.unloadWarzone(warzoneName)) {
+            plugin.playerManager.sendMessage(sender, "Warzone $warzoneName does not exist.")
+        } else {
+            plugin.playerManager.sendMessage(sender, "Warzone unloaded.")
+            when (val warzoneResult = plugin.warzoneManager.loadWarzone(warzoneName)) {
+                is Ok -> {
+                    plugin.playerManager.sendMessage(sender, "Warzone loaded.")
+                    plugin.warzoneManager.addWarzone(warzoneResult.value)
+                }
+                is Err -> plugin.playerManager.sendMessage(sender, "Error when loading warzone: ${warzoneResult.error}")
+            }
+        }
+    }
+
+    override fun execute(plugin: WarPlus, sender: CommandSender, args: List<String>): Boolean {
+        if (args.isEmpty()) {
+            resetPlugin(plugin, sender)
+        } else {
+            resetWarzone(plugin, sender, args[0])
+        }
         return true
     }
 
     override fun tab(plugin: WarPlus, sender: CommandSender, args: List<String>): List<String> {
-        return emptyList()
+        return plugin.warzoneManager.getWarzoneNames().filter {
+            it.startsWith(args[0].toLowerCase())
+        }
     }
 }
