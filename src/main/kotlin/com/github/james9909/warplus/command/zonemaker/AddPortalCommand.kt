f662750971843c7e5bdc603fa588493e294456cd
James Wang <jameswang9909@hotmail.com>
2021-02-14 12:54:53 -0500

    Allow zonemakers to specify portal location through the command arguments

--- src/main/kotlin/com/github/james9909/warplus/command/zonemaker/AddPortalCommand.kt
+++ src/main/kotlin/com/github/james9909/warplus/command/zonemaker/AddPortalCommand.kt
@@ -4,11 +4,22 @@ import com.github.james9909.warplus.WARPLUS_BASE_COMMAND
 import com.github.james9909.warplus.WarPlus
 import com.github.james9909.warplus.extensions.blockLocation
 import com.github.james9909.warplus.structures.WarzonePortalStructure
+import org.bukkit.Location
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
+private fun getYawFromString(direction: String): Float? {
+    return when (direction) {
+        "n" -> 180F
+        "e" -> 270F
+        "s" -> 0F
+        "w" -> 90F
+        else -> null
+    }
+}
+
 class AddPortalCommand : ZonemakerCommand() {
-    override val usageString = "/$WARPLUS_BASE_COMMAND addportal <warzone name> <portal name>"
+    override val usageString = "/$WARPLUS_BASE_COMMAND addportal <warzone name> <portal name> [<x> <y> <z> <n|e|s|w>]"
     override val description = "Create a warzone portal at the current location"
 
     override fun execute(plugin: WarPlus, sender: CommandSender, args: List<String>): Boolean {
@@ -22,13 +33,23 @@ class AddPortalCommand : ZonemakerCommand() {
             plugin.playerManager.sendMessage(sender, "That warzone doesn't exist")
             return true
         }
-        val origin = sender.location.blockLocation()
         val existingPortal = warzone.getPortals().any { portal ->
             portal.name.equals(args[1], true)
         }
         if (existingPortal) {
             plugin.playerManager.sendMessage(sender, "A portal with that name already exists")
             return true
+        }
+        val origin = if (args.size == 6) {
+            val yaw = getYawFromString(args[5])
+            if (yaw == null) {
+                plugin.playerManager.sendMessage(sender, "The direction must be either n, e, s, or w")
+                return true
+            }
+            // Add 180 to the yaw so that the sign ends up facing the direction specified by the user
+            Location(sender.world, args[2].toDouble(), args[3].toDouble(), args[4].toDouble(), yaw + 180, 0F)
+        } else {
+            sender.location.blockLocation()
         }
         val newPortal = WarzonePortalStructure(plugin, origin, args[1], warzone)
         val overlappingPortal = plugin.warzoneManager.getWarzones().any {
