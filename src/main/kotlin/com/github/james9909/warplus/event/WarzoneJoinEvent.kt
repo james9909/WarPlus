2dca1bb0a6e1713db3d5fb024806c87b86823619
James Wang <jameswang9909@hotmail.com>
2021-03-25 19:38:42 -0500

    Allow warzone joins to be cancelled through the event

--- src/main/kotlin/com/github/james9909/warplus/event/WarzoneJoinEvent.kt
+++ src/main/kotlin/com/github/james9909/warplus/event/WarzoneJoinEvent.kt
@@ -2,12 +2,14 @@ package com.github.james9909.warplus.event
 
 import com.github.james9909.warplus.Warzone
 import org.bukkit.entity.Player
+import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.HandlerList
 
 private val HANDLERS = HandlerList()
 
-class WarzoneJoinEvent(val player: Player, val warzone: Warzone) : Event() {
+class WarzoneJoinEvent(val player: Player, val warzone: Warzone) : Cancellable, Event() {
+    private var cancelled = false
 
     override fun getHandlers(): HandlerList {
         return HANDLERS
@@ -18,5 +20,13 @@ class WarzoneJoinEvent(val player: Player, val warzone: Warzone) : Event() {
         fun getHandlerList(): HandlerList {
             return HANDLERS
         }
+    }
+
+    override fun isCancelled(): Boolean {
+        return cancelled
+    }
+
+    override fun setCancelled(cancel: Boolean) {
+        cancelled = cancel
     }
 }
