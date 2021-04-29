6a1ec2888af6a53285dbfd146dd8ac459f297634
James Wang <jameswang9909@hotmail.com>
2021-04-28 20:25:51 -0500

    Add PlayerEquipClassEvent

---
+++ src/main/kotlin/com/github/james9909/warplus/event/PlayerEquipClassEvent.kt
@@ -0,0 +1,22 @@
+package com.github.james9909.warplus.event
+
+import com.github.james9909.warplus.WarClass
+import com.github.james9909.warplus.Warzone
+import org.bukkit.entity.Player
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+private val HANDLERS = HandlerList()
+
+class PlayerEquipClassEvent(val player: Player, val warzone: Warzone, val className: String) : Event() {
+    override fun getHandlers(): HandlerList {
+        return HANDLERS
+    }
+
+    companion object {
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return HANDLERS
+        }
+    }
+}
